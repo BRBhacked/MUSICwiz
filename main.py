73ba1e7c0d44da84db3711e363fede9cc7307627
araina25 <araina1@ualberta.ca>
2023-01-07 15:54:08 -0700

    Adding comments to the file

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+#This code implements spotipy to implement the data from spotify on python
+#And create a user freindly way to give a music reccomendation 
+
+#Installing neccesaary python libraries to implement the program 
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -6,10 +10,12 @@ import spotipy
 import os
 
 
+#Reading the dataset files
 spotify_data = pd.read_csv('./data/data.csv.zip')
 genre_data = pd.read_csv('./data/data_by_genres.csv')
 data_by_year = pd.read_csv('./data/data_by_year.csv')
 
+#We are using plotly to visualise (Using a line graph) the values of different audios.
 import plotly.express as px 
 sound_features = ['acousticness', 'danceability', 'energy', 'instrumentalness', 'liveness', 'valence']
 fig = px.line(data_by_year, x='year', y=sound_features)
