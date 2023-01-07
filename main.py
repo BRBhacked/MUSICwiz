dd4b98ff52e26e4501a48943a80b3160af2e89df
fa1ak <fsethi@ualberta.ca>
2023-01-07 15:57:18 -0700

    adding characteristics of different genres

--- main.py
+++ main.py
@@ -2,13 +2,13 @@
 #And create a user freindly way to give a music reccomendation 
 
 #Installing neccesaary python libraries to implement the program 
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import spotipy
 import os
-
 
 #Reading the dataset files
 spotify_data = pd.read_csv('./data/data.csv.zip')
@@ -19,4 +19,8 @@ data_by_year = pd.read_csv('./data/data_by_year.csv')
 import plotly.express as px 
 sound_features = ['acousticness', 'danceability', 'energy', 'instrumentalness', 'liveness', 'valence']
 fig = px.line(data_by_year, x='year', y=sound_features)
+fig.show()
+
+top10_genres = genre_data.nlargest(10, 'popularity')
+fig = px.bar(top10_genres, x='genres', y=['valence', 'energy', 'danceability', 'acousticness'], barmode='group')
 fig.show()
