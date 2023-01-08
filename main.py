cfa6cff444a2797b5512c37e1db1efc93a22ba5f
Agrim Sood <agrimsood@gmail.com>
2023-01-08 01:23:36 -0700

    get song data function half implementation

--- main.py
+++ main.py
@@ -93,3 +93,16 @@ import plotly.express as px
 fig = px.scatter(projection, x='x', y='y', color='cluster', hover_data=['x', 'y', 'title'])
 fig.show()
 #hi
+from collections import defaultdict
+from scipy.spatial.distance import cdist
+import difflib
+number_cols = ['valence','year','acousticness', 'danceability', 'duration_ms', 'energy', 'explicit',
+ 'instrumentalness', 'key', 'liveness', 'loudness', 'mode', 'popularity', 'speechiness', 'tempo']
+
+def get_song_data(song,spotify_data)
+'''gets all the song details for the selected songs.
+Song takes in input as key value pairs dictionary in form of name and year'''
+    try:
+        song_data = spotify_data[(spotify_data['name'] == song['name']) 
+                                & (spotify_data['year'] == song['year'])].iloc[0]
+        return song_data
