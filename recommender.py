7dd713165f827b798f14e26ab4df2869cf176727
Agrim Sood <agrimsood@gmail.com>
2023-01-08 01:18:29 -0700

    get song data function

--- recommender.py
+++ recommender.py
@@ -1,5 +1,13 @@
+
 from collections import defaultdict
 from scipy.spatial.distance import cdist
 import difflib
 number_cols = ['valence','year','acousticness', 'danceability', 'duration_ms', 'energy', 'explicit',
  'instrumentalness', 'key', 'liveness', 'loudness', 'mode', 'popularity', 'speechiness', 'tempo']
+
+def get_song_data(song,spotify_data)
+'''gets all the song details for the selected songs. Song takes in input as key value pairs dictionary in form of name and year'''
+try:
+   song_data = spotify_data[(spotify_data['name'] == song['name']) & (spotify_data['year'] == song['year'])].iloc[0]
+    
+    return song_data
